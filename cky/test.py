7695465ab6d2574fc78bac14dd69d7d1a389ae51
daandouwe <daan_douwe@outlook.com>
2018-11-30 14:26:09 +0100

    New sources.

--- cky/test.py
+++
@@ -1,6 +0,0 @@
-def incoming(left, right):
-    """Return all incoming nodes to a node that spans left to right."""
-    for split in range(left+1, right):
-        for label in range(3):
-            yield (left, split, label)
-            yield (split, right, label)
