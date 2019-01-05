181c75f6adafafd1cbe2f336b7847958fe7113d9
daandouwe <daan_douwe@outlook.com>
2019-01-05 22:58:03 +0100

    Lisa scripts are working.

--- lisa/train-gpu.sh
+++ lisa/train-gpu.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # we the seed in the first argument, the output dir in the second, and all else goes to the argument parser
-SEED=$(($1))
-OUTPUT_DIR=$2
-PARSER_ARGS="${@:3}"
+GPU=$(($1))
+SEED=$(($2))
+OUTPUT_DIR=$3
+PARSER_ARGS="${@:4}"
 
 export LANGUAGE="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export PYTHONIOENCODING="UTF-8"
 export PYTHONUNBUFFERED="1"
-export MKL_NUM_THREADS=1
 
 module load Python/3.6.3-foss-2017b
+
+# activate gpu environment
+source ${HOME}/envs/dynet-gpu/bin/activate
 
 TIMESTAMP=`date +%s`
 echo "`date` Training starts"
@@ -20,7 +23,7 @@ echo "`date` Training starts"
 which python
 python --version
 
-python src/main.py train --dynet-autobatch 1 --dynet-seed ${SEED} --numpy-seed ${SEED} ${PARSER_ARGS[@]} &>> ${OUTPUT_DIR}/train${SEED}.log &
+python src/main.py train --dynet-autobatch 1 --dynet-gpus 1 --dynet-devices GPU:${GPU} --dynet-seed ${SEED} --numpy-seed ${SEED} ${PARSER_ARGS[@]} &>> ${OUTPUT_DIR}/train${SEED}.log &
 
 wait
 
