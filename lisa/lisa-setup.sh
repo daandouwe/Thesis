af049e4393334d485a5017c344ddba6904d25937
daandouwe <daan_douwe@outlook.com>
2019-01-15 19:24:33 +0100

    New jobs

---
+++ lisa/lisa-setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# get the encoding right
+export LANG="en_US.UTF-8"
+export LANGUAGE="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+
+# load the proper python module
+module load Python/3.6.3-foss-2017b
